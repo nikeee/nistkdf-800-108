b4079adaf1f37f7b81f2840d305bdb0666fe6981
Niklas Mollenhauer <nikeee@outlook.com>
2025-01-18 01:13:09 +0100

    Assert number of tests executed

--- src/counter.test.js
+++ src/counter.test.js
@@ -34,10 +34,13 @@ describe("counting KBKDF", async () => {
 			expect(byCounterLocation.BEFORE_FIXED?.length).toBeGreaterThan(0);
 			expect(Object.keys(byRLen)).toStrictEqual(["8", "16", "24", "32"]);
 
+			expect(Object.keys(byRLen).length).toBe(4);
 			for (const [counterWidth, suites] of Object.entries(byRLen)) {
 				it(`${counterWidth} bit r length`, async () => {
+					expect(suites?.length).toBe(1);
 					// @ts-expect-error
 					for (const suite of suites) {
+						expect(suite.tests.length).toBe(40);
 						for (const test of suite.tests) {
 							const n = test.COUNT + 1;
 
@@ -74,10 +77,13 @@ describe("counting KBKDF", async () => {
 			expect(byCounterLocation.AFTER_FIXED?.length).toBeGreaterThan(0);
 			expect(Object.keys(byRLen)).toStrictEqual(["8", "16", "24", "32"]);
 
+			expect(Object.keys(byRLen).length).toBe(4);
 			for (const [counterWidth, suites] of Object.entries(byRLen)) {
 				it(`${counterWidth} bit r length`, async () => {
+					expect(suites?.length).toBe(1);
 					// @ts-expect-error
 					for (const suite of suites) {
+						expect(suite.tests.length).toBe(40);
 						for (const test of suite.tests) {
 							const n = test.COUNT + 1;
 
@@ -114,10 +120,13 @@ describe("counting KBKDF", async () => {
 			expect(byCounterLocation.MIDDLE_FIXED?.length).toBeGreaterThan(0);
 			expect(Object.keys(byRLen)).toStrictEqual(["8", "16", "24", "32"]);
 
+			expect(Object.keys(byRLen).length).toBe(4);
 			for (const [counterWidth, suites] of Object.entries(byRLen)) {
 				it(`${counterWidth} bit r length`, async () => {
+					expect(suites?.length).toBe(1);
 					// @ts-expect-error
 					for (const suite of suites) {
+						expect(suite.tests.length).toBe(40);
 						for (const test of suite.tests) {
 							const n = test.COUNT + 1;
 
